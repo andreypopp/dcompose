31b260284c71c305aed84dd928ed8e6da851ee5a
Andrey Popp <8mayday@gmail.com>
2013-10-08 02:47:50 +0400

    fix middleware

--- middleware.js
+++ middleware.js
@@ -1,27 +1,37 @@
 var aggregate = require('stream-aggregate-promise'),
-    mimetype  = require('mimetype').lookup;
+    mimetype  = require('mimetype');
+
+function aggregateStreams(streams) {
+  var result = {};
+  for (var k in streams)
+    result[k] = aggregate(streams[k]);
+  return result;
+}
 
 module.exports = function(composer) {
-  var bundle = composer.bundle();
-
-  composer.on('update', function() {
-    bundle = composer.bundle();
-  });
-
-  return function(req, res, next) {
-    bundle
+  function serveBundle(req, res, next) {
+    serveBundle.bundle
       .then(function(bundles) {
-        var bundleName = req.params[0],
+        var bundleName = req.url.slice(1),
             bundle = bundles[bundleName];
 
         if (!bundle) return next();
 
-        res.setHeader('Content-Type', mimetype(bundleName));
-        return aggregate(bundle);
+        res.setHeader('Content-Type', mimetype.lookup(bundleName));
+        return bundle;
       })
       .then(function(bundle) {
         res.send(bundle);
       })
       .fail(next);
   }
+
+  serveBundle.build = function() {
+    serveBundle.bundle = composer.bundle().then(aggregateStreams);
+  }
+
+  serveBundle.build();
+  composer.on('update', serveBundle.build);
+
+  return serveBundle;
 }
