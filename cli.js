70296b5feab883da5fd22f25ff9cdb8da85db46f
Andrey Popp <8mayday@gmail.com>
2013-11-05 23:21:45 +0400

    Move cli execution to cli module

--- cli.js
+++ cli.js
@@ -1,4 +1,6 @@
-module.exports = require('optimist')
+"use strict";
+
+var entry = require('optimist')
     .usage('Usage: dcompose [options] entry')
     .option('h', {
       alias: 'help',
@@ -45,3 +47,67 @@ module.exports = require('optimist')
     .option('extension', {
       describe: 'File extensions to treat as modules [default: .js]'
     });
+
+function run(argv) {
+  argv = argv || entry.argv;
+
+  if (argv.help)
+    return entry.showHelp();
+
+  if (argv.version)
+    return console.log(version);
+
+  if (argv._.length === 0)
+    error('provide bundle entry module as an argument', true);
+
+  if (argv.watch && !argv.output)
+    error('provide output via -o/--output option', true);
+
+  var output = argv.output || argv.o;
+  var transform = [].concat(argv.t).concat(argv.transform);
+  var cssTransform = argv['css-transform'];
+  var globalTransform = argv['global-transform'];
+
+  var fs          = require('fs'),
+      combine     = require('stream-combiner'),
+      common      = require('./common'),
+      version     = require('./package.json').version,
+      dcompose    = require('./index');
+
+  var composer = dcompose(argv._, {
+    transform: transform,
+    cssTransform: cssTransform,
+    globalTransform: globalTransform,
+    extensions: [].concat(argv.extension).filter(Boolean),
+    debug: argv.debug,
+    watch: argv.watch
+  });
+
+  if (argv.watch) {
+    console.log('starting to watch for source changes');
+    composer.on('update', function() {
+      console.log('source change detected, rebuilding bundle');
+      bundle();
+    });
+  }
+
+  bundle();
+
+  function bundle() {
+    var out = output ? fs.createWriteStream(output) : process.stdout;
+    if (argv.js)
+      combine(composer.bundleJS(), out)
+    else if (argv.css)
+      combine(composer.bundleCSS(), out)
+    else
+      combine(composer.bundle(), out);
+  }
+}
+
+function error(msg, showHelp) {
+  if (showHelp) entry.showHelp();
+  console.warn('error: ', msg);
+  process.exit(1);
+}
+
+module.exports = {run: run, entry: entry};
